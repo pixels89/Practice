0b6412730d64e72f7ef23c59922c3721d7bd4ed9
Mukesh Barman <mbarman@salesforce.com>
2019-02-21 23:46:09 +0530

    added anagram quesiton

--- Algo/src/in/barmans/practice/Anagram.java
+++ Algo/src/in/barmans/practice/Anagram.java
@@ -2,6 +2,11 @@ package in.barmans.practice;
 
 /**
  * 
+ * Given two strings in lowercase, the task is to make them anagram. The only
+ * allowed operation is to remove a character from any string. Find minimum
+ * number of characters to be deleted to make both the strings anagram? If two
+ * strings contains same data set in any order then strings are called Anagrams.
+ * 
  * @author mbarman
  *
  */
