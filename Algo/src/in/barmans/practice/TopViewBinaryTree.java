5f02681d20e5beb70759b4ba2ededcae46ba6147
Mukesh <mbarman@salesforce.com>
2019-03-02 22:54:38 +0530

    Two ways to get top view of BT  1. Checking both height index and width index of element  2. Tracking width index per level

--- Algo/src/in/barmans/practice/TopViewBinaryTree.java
+++ Algo/src/in/barmans/practice/TopViewBinaryTree.java
@@ -1,9 +1,8 @@
 package in.barmans.practice;
 
 import java.util.Collection;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.TreeMap;
 
 public class TopViewBinaryTree {
@@ -19,27 +18,27 @@ public class TopViewBinaryTree {
 		}
 	}
 
-	static class NodeDepth {
+	static class NodeWithIndex {
 		Node node;
-		int depth;
+		int index;
 
-		NodeDepth(Node node, int depth) {
-			this.depth = depth;
+		NodeWithIndex(Node node, int index) {
+			this.index = index;
 			this.node = node;
 		}
 	}
 
-	static TreeMap<Integer, NodeDepth> map = new TreeMap<>();
+	static TreeMap<Integer, NodeWithIndex> map = new TreeMap<>();
 
 	public static void topViewInternal(Node node, int hIndex, int depth) {
 
 		if (node != null) {
 
 			if (!map.containsKey(hIndex)) {
-				map.put(hIndex, new NodeDepth(node, depth));
+				map.put(hIndex, new NodeWithIndex(node, depth));
 			} else {
-				if (map.get(hIndex).depth > depth) {
-					map.put(hIndex, new NodeDepth(node, depth));
+				if (map.get(hIndex).index > depth) {
+					map.put(hIndex, new NodeWithIndex(node, depth));
 				}
 			}
 
@@ -52,8 +51,38 @@ public class TopViewBinaryTree {
 
 	private static void topView(Node node) {
 		topViewInternal(node, 0, 0);
-		Collection<NodeDepth> values = map.values();
-		for (NodeDepth nodeDepth : values) {
+		Collection<NodeWithIndex> values = map.values();
+		for (NodeWithIndex nodeDepth : values) {
+			System.out.println(nodeDepth.node.data);
+		}
+	}
+
+	private static void topViewLevelWiseInternal(Node node, int hIndex) {
+		if (node != null) {
+			Queue<NodeWithIndex> q = new LinkedList<>();
+			q.add(new NodeWithIndex(node, hIndex));
+			while (!q.isEmpty()) {
+				NodeWithIndex qNode = q.remove();
+				Node newNode = qNode.node;
+
+				if (!map.containsKey(qNode.index)) {
+					map.put(qNode.index, qNode);
+				}
+
+				if (newNode.left != null) {
+					q.add(new NodeWithIndex(newNode.left, qNode.index - 1));
+				}
+				if (newNode.right != null) {
+					q.add(new NodeWithIndex(newNode.right, qNode.index + 1));
+				}
+			}
+		}
+	}
+
+	private static void topViewLevelWise(Node node) {
+		topViewLevelWiseInternal(node, 0);
+		Collection<NodeWithIndex> values = map.values();
+		for (NodeWithIndex nodeDepth : values) {
 			System.out.println(nodeDepth.node.data);
 		}
 	}
@@ -85,6 +114,6 @@ public class TopViewBinaryTree {
 		root.right.left = new Node(6);
 		root.right.right = new Node(7);
 
-		topView(root);
+		topViewLevelWise(root);
 	}
 }
